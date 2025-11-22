a909e080e24b8e56d16a482837b6ed3f95ae3ce0
Nathan Kleiman <natecsun@yahoo.com>
2025-11-22 09:20:40 -0600

    Fix: Externalize pdf-parse in webpack config

--- next.config.js
+++ next.config.js
@@ -36,6 +36,13 @@ const nextConfig = {
         ]
       }
     ]
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('pdf-parse');
+    }
+    return config;
   }
 }
 
